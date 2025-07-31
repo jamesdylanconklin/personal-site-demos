1e5a1449d2fd9b3aee2f6d0aef81abf8790c875b
James Conklin <jamesdylanconklin@gmail.com>
2025-07-30 17:37:27 -0700

    define input variables

---
+++ demos/lambda/s3-fetch/variables.tf
@@ -0,0 +1,25 @@
+variable "parent_api_id" {
+  description = "The ID of the parent REST API"
+  type        = string
+}
+
+variable "parent_resource_id" {
+  description = "The ID of the parent resource in the REST API"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Name of the project, used for resource naming and tagging"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket to provision if absent and then fetch from"
+  type        = string
+}
